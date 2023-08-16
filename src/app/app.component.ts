8bf42ab22578c63a71c060c4a632e375430fba68
beluyencode <longlc956@gmail.com>
2023-08-16 22:26:33 +0700

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,34 @@ export class AppComponent {
   coment_id = '';
   name_id = '';
   disabled = true;
-  ignore = ["Kiếm", "tiền", "Nợ", 'xấu Xổ', "số", "FE CREDIT", "Tiền", "Nợ", "Đắt", "Rẻ"]
+  ignore = ["Kiếm", "tiền", "Nợ", 'xấu Xổ', "số", "FE CREDIT", "Tiền", "Nợ", "Đắt", "Rẻ"];
+  goalsArray = [
+    "Đầu tư thành công vào dự án mới",
+    "Tiết kiệm đủ để mua căn nhà mới",
+    "Đi du lịch Châu Âu",
+    "Lần đầu triển khai dự án kinh doanh quy mô lớn",
+    "Mua sắm cho gia đình thoải mái",
+    "Dành thời gian đưa con đi trải nghiệm cuộc sống",
+    "Một mình đi leo núi",
+    "Chinh phục đỉnh Everest",
+    "Tham gia giải London Marathon 42km",
+    "Dành 1 tháng cho bản thân nghỉ ngơi",
+    "Tặng mẹ 1 chiếc xe mới",
+    "Mua ngôi nhà mới đón em bé",
+    "Tiết kiệm cho con đi du học",
+    "Có thêm nhiều thời gian cho con",
+    "Đưa gia đình đi du lịch nhiều hơn",
+    "Chọn đúng cổ phiếu đầu tư",
+    "Triển khai ý tưởng kinh doanh mới",
+    "Nghỉ hưu sớm ở tuổi 40",
+    "Sang Mỹ làm việc và lập công ty",
+    "Sky diving",
+    "Triển khai dự án xã hội thành công",
+    "Giúp đỡ thêm nhiều trẻ em vùng cao",
+    "Công ty được niêm yết trên sàn chứng khoán",
+    "Xây dựng công ty vững mạnh",
+    "Mua sắm đồ Tiffany & Co."
+  ];
   constructor(
     private toastr: ToastrService,
     private http: HttpClient
@@ -28,7 +55,7 @@ export class AppComponent {
   }
 
   check(event: any) {
-    if (this.comment.length >= 4 && this.name.length >= 4) {
+    if (this.comment.length >= 4) {
       this.disabled = false;
     } else {
       this.disabled = true;
@@ -36,6 +63,21 @@ export class AppComponent {
   }
 
   submit() {
+    // this.goalsArray.forEach((ele: any) => {
+    //   this.http.post('https://api.thoikhacgiaothoi.com/api/v1/answers', [
+    //     {
+    //       question_id: this.coment_id,
+    //       answers: [ele]
+    //     }, {
+    //       question_id: this.name_id,
+    //       answers: ['demo']
+    //     },
+    //   ]).subscribe((res) => {
+    //     // if (res) {
+    //     //   this.isSubmit = true;
+    //     // }
+    //   })
+    // })
     if (!this.disabled) {
       this.http.post('https://api.thoikhacgiaothoi.com/api/v1/answers', [
         {
